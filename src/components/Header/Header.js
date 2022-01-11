03b019afb079f9fd5108b1c4f212d9099c923d6d
unknown <mdmehedyh55@gamil.com>
2022-01-11 21:48:45 +0600

    link to add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,13 +12,13 @@ const Header = () => {
             </div>
             <Navbar bg="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand className="link-header-tag" href="/">ema-John</Navbar.Brand>
+                    <Navbar.Brand className="link-header-tag" to="/">ema-John</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Link className="link-header-tag" href="/home">Shop</Link>
-                            <Link className="link-header-tag" href="/review">Order Review</Link>
-                            <Link className="link-header-tag" href="/manage">Manage Inventory here</Link>
+                            <Link className="link-header-tag" to="/home">Shop</Link>
+                            <Link className="link-header-tag" to="/review">Order Review</Link>
+                            <Link className="link-header-tag" to="/manage">Manage Inventory here</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
