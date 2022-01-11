e01b26296771c265643bf00f7b57ef63a017630a
unknown <mdmehedyh55@gamil.com>
2022-01-11 21:40:42 +0600

    add link tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Header.css';
 import loto from '../../images/logo.png'
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
             <div className="text-center">
-                <img style={{height: '80px'}} src={loto} alt="" />
+                <img style={{ height: '80px' }} src={loto} alt="" />
             </div>
-            <nav>
-                <ul>
-                    <a href="/home">Shop</a>
-                    <a href="/review">Order Review</a>
-                    <a href="/manage">Manage Inventory here</a>
-                </ul>
-            </nav>
+            <Navbar bg="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand className="link-header-tag" href="/">ema-John</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link className="link-header-tag" href="/home">Shop</Nav.Link>
+                            <Nav.Link className="link-header-tag" href="/review">Order Review</Nav.Link>
+                            <Nav.Link className="link-header-tag" href="/manage">Manage Inventory here</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
